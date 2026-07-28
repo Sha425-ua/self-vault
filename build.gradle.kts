d28b3d3cc8dd60f3e81f76ebe91deb98765e196a
vdehtiarenko <vdehtiarenko@proton.me>
2026-07-28 16:52:38 +0300

    feat: update Java compatibility settings to version 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,9 +15,8 @@ subprojects {
     }
 
     configure<JavaPluginExtension> {
-        toolchain {
-            languageVersion.set(JavaLanguageVersion.of(21))
-        }
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     tasks.withType<Test> {
